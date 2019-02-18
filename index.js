610f3d53209c9a29a607a60d608fdc2630bdcfe1
Gilbert Torchon <gilbert@noukod.com>
2019-02-18 18:30:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,5 +31,5 @@ function showResults(jsonResponse){
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return 'e14e3a67b5f77a6b5801606c3ad364a8e355fff7'
+  return ''
 }
