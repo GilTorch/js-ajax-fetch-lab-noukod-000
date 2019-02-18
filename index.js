30ada5b9432397049c370aca58ef77caf92c09e8
Gilbert Torchon <gilbert@noukod.com>
2019-02-18 18:04:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function forkRepo() {
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  return 'e14e3a67b5f77a6b5801606c3ad364a8e355fff7'
 }
