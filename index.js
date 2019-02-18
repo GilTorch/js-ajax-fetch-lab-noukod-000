44525b426afe7c472af41685d8a85efdc114cfdc
Gilbert Torchon <gilbert@noukod.com>
2019-02-18 18:31:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
   fetch(`https://api.github.com/repos/${repo}/forks`,{
-    method:"POST",
+    method:'POST',
     headers:{
       Authorization:`token ${getToken()}`
     }
